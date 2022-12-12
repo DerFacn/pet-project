ac8cd1f5bf43b1e2ff0ad369ea0514e6431b4d8c
derfacn <nikitalupalo145@gmail.com>
2022-12-13 00:17:49 +0100

    Структурка и дефолт шаблон

---
+++ backend/work/default/myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'myapp/index.html')
+
+def about(request):
+    return render(request, 'myapp/about.html')
