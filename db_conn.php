24c1cc0959c99cac56a5e72ba30be133c32b448b
themanwhocan <120328517+themanwhocan@users.noreply.github.com>
2022-12-12 23:39:35 +0300

    Revert "внедрение шаблона формы login.php"

--- db_conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function setup( array $login_data = ["localhost", "root", "mysql", "pet-project"]) //$host, $user, $password, $db_name)
-{
-    return mysqli_connect( ...$login_data );// $host, $user, $password, $db_name);
-}
-
-?>
